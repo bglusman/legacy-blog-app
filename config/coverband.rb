a8ff4538ea8e7285c2ce2653cb933aace71ac976
Fabio Rehm <fgrehm@gmail.com>
2014-07-25 16:34:18 -0300

    Enable coverband on heroku at all times

--- config/coverband.rb
+++ config/coverband.rb
@@ -6,11 +6,13 @@ Coverband.configure do |config|
   config.coverage_baseline = baseline
   # config.root_paths        = ['/app/']
   config.ignore            = ['vendor']
-  # Since rails and other frameworks lazy load code. I have found it is bad to allow
-  # initial requests to record with coverband. This ignores first 10 requests
   config.startup_delay     = Rails.env.production? ? 10 : 1
   config.percentage        = Rails.env.production? ? 15.0 : 100.0
   config.verbose           = Rails.env.production? ? false : true
+  # TODO: GET RID OF THIS
+  config.startup_delay     = 1
+  config.percentage        = 100.0
+  config.verbose           = true
   if defined? Redis
     config.redis = Redis.new
   end
