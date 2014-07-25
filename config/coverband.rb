6864b4bf6f968e44e1ad54b5eb894b5b794b39e5
Fabio Rehm <fgrehm@gmail.com>
2014-07-25 19:07:00 -0300

    Revert "Give the heroku app a delay of 10 requests as an attempt to make it pick up the models"
    This reverts commit de5f7b04618a5c31ac4fd41865074ae3a6257c43.

--- config/coverband.rb
+++ config/coverband.rb
@@ -7,11 +7,12 @@ Coverband.configure do |config|
   config.root_paths        = ['/app/']
   config.ignore            = ['vendor']
 
-  config.startup_delay     = Rails.env.production? ? 10 : 1
   # This is what a real app setup would look like
+  config.startup_delay     = Rails.env.production? ? 10 : 1
   config.percentage        = Rails.env.production? ? 15.0 : 100.0
   config.verbose           = Rails.env.production? ? false : true
   # But we just want to try out the gem
+  config.startup_delay     = 1
   config.percentage        = 100.0
   config.verbose           = true
 
