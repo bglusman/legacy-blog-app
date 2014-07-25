1bae2aa002f6a5b1ca8733f52d8a94983925b551
Fabio Rehm <fgrehm@gmail.com>
2014-07-25 18:19:44 -0300

    Notes on coverband config

--- config/coverband.rb
+++ config/coverband.rb
@@ -6,10 +6,12 @@ Coverband.configure do |config|
   config.coverage_baseline = baseline
   config.root_paths        = ['/app/']
   config.ignore            = ['vendor']
+
+  # This is what a real app setup would look like
   config.startup_delay     = Rails.env.production? ? 10 : 1
   config.percentage        = Rails.env.production? ? 15.0 : 100.0
   config.verbose           = Rails.env.production? ? false : true
-  # TODO: GET RID OF THIS
+  # But we just want to try out the gem
   config.startup_delay     = 1
   config.percentage        = 100.0
   config.verbose           = true
