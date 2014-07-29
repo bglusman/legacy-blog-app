0075301b5c622ad06fac0f12bb2b4b9e73c31943
Fabio Rehm <fgrehm@gmail.com>
2014-07-29 00:27:57 -0300

    Add some scopes to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ActiveRecord::Base
   belongs_to :category
   validates :title, :body, presence: true
+
+  # This will look like it has been used
+  scope :not_highlighted, -> { where(highlighted: false) }
+
+  # But this won't because there is a line break
+  scope :highlighted, -> {
+    where(highlighted: true)
+  }
 end
