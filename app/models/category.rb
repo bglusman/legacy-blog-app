14f96c910965dc1509d4fad1687cdfde0c66801e
Fabio Rehm <fgrehm@gmail.com>
2014-07-29 00:16:18 -0300

    Crazy stuff, just to bring coverage down

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ActiveRecord::Base
+  validates :name, presence: true
+
+  # Crazy stuff, just to bring coverage down
+  before_save do
+    self.name = self.name.mb_chars.upcase
+  end
 end
